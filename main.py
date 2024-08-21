267f7d3f9b7c4bdab7bdc00f8515416ada7c92b0
jonnyjackson26 <jrsjackson26@gmail.com>
2024-08-21 16:55:33 -0600

    added important dates

--- main.py
+++ main.py
@@ -5,11 +5,32 @@ import calendar
 from datetime import datetime, timedelta
 import os
 
+class ImportantDate:
+    def __init__(self, month, day, text):
+        self.month = month
+        self.day = day
+        self.text = text
+
+
+importantDates = [
+    ImportantDate(9, 26, "Jonny's birthday"),
+    ImportantDate(1, 13, "Jessy's birthday"),
+    ImportantDate(1, 16, "Dad's birthday"),
+    ImportantDate(8, 27, "Jacobs's birthday"),
+    ImportantDate(8, 15, "Gloria's birthday"),
+    ImportantDate(12, 26, "Christmas"),
+    ImportantDate(10, 27, "Thanksgiving"),
+    ImportantDate(2, 13, "Valentines Day"),
+    ImportantDate(7, 4, "Fouth of July"),
+    ImportantDate(7, 3, "Mom's birthday"),
+]
+
 # Missionary details
 missionaryName = "Jonathan Jackson"
 missionName = "Dominican Republic, Santo Domingo West Mission"
 isElder = False
 startDate = datetime(2024, 10, 1)  # October 2024 (year, month, day)
+
 
 # Directory containing images
 pics_folder = 'pics'
@@ -62,7 +83,16 @@ def draw_month(c, year, month):
             
             # Add the day number in the top left of each box
             if month_cal[week][day] != 0:
-                c.drawString(x + 0.1 * inch, y + cell_height - 0.2 * inch, str(month_cal[week][day]))
+                day_number = str(month_cal[week][day])
+                c.drawString(x + 0.1 * inch, y + cell_height - 0.2 * inch, day_number)
+                
+                # Draw important dates text if it matches
+                for important_date in importantDates:
+                    if important_date.month == month and important_date.day == month_cal[week][day]:
+                        c.setFont("Helvetica-Oblique", 8)
+                        c.drawString(x + 0.1 * inch, y + cell_height / 2, important_date.text)
+                        c.setFont("Helvetica", 12)  # Reset font after drawing important dates
+
 
 
 def add_image_page(c, image_path):
